0d5fe39f148ddb3b5870a885434ab8f052ec0e73
midhundas123 <midhundas123@gmail.com>
2020-04-12 19:46:49 +0530

    2

--- src/test/java/TestScripts/RegressionTest.java
+++ src/test/java/TestScripts/RegressionTest.java
@@ -15,4 +15,13 @@ public class RegressionTest extends TestHelper{
        // String z=  y.gettextofdiplaymessage();
         Assert.assertTrue(true);
     }
+
+    @Test
+    public void VerifyLogin1(){
+        login x = new login(driver);
+        x.login("un","psd");
+        Homepage y = new Homepage(driver);
+        // String z=  y.gettextofdiplaymessage();
+        Assert.assertTrue(true);
+    }
 }
